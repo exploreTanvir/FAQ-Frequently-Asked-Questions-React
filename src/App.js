10d2ad1953f66257a2d82ab99a9287e9d0ab2c68
Tanvir_Hossen <tanvirhossen853@gmail.com>
2023-10-31 00:55:57 +0600

    FAQS (Freequently Asked Questions)

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FAQS from './FAQS (Freequently Asked Questions)/FAQS'
+
+const App = () => {
+  return (
+    <div>
+      <FAQS/>
+    </div>
+  )
+}
+
+export default App
